bef7ce91702164ae0898becc78453c72ae40d99b
alexander123 <alekeycastillo1@gmail.com>
2026-04-01 17:15:29 -0500

    cambios en el idEstado y eliminacion de doble filtro redundante

--- js/getPeliculas.js
+++ js/getPeliculas.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDFGTkxM0Jd5uQzuTDRz_vkk_ssoE4WZTw",
@@ -7,7 +13,7 @@ const firebaseConfig = {
   projectId: "fir-fdae7",
   storageBucket: "fir-fdae7.firebasestorage.app",
   messagingSenderId: "637019613644",
-  appId: "1:637019613644:web:0e568be0dfd450f5511569"
+  appId: "1:637019613644:web:0e568be0dfd450f5511569",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -27,10 +33,10 @@ async function getPeliculas() {
 
   if (tipo === "cartelera") {
     titulo = "Cartelera";
-    idEstado = "1";
+    idEstado = 1;
   } else if (tipo === "estrenos") {
     titulo = "Próximos Estrenos";
-    idEstado = "2";
+    idEstado = 2;
   }
 
   contenedor.innerHTML = `<br/><h1>${titulo}</h1><br/><p>Cargando...</p>`;
@@ -41,7 +47,7 @@ async function getPeliculas() {
     if (idEstado) {
       peliculasQuery = query(
         collection(db, "peliculas"),
-        where("idEstado", "==", idEstado)
+        where("idEstado", "==", idEstado),
       );
     } else {
       peliculasQuery = collection(db, "peliculas");
@@ -59,9 +65,6 @@ async function getPeliculas() {
     snapshot.forEach((doc) => {
       const p = doc.data();
       const docId = doc.id;
-      // Filtrar por idEstado si se especifico
-      if (idEstado && String(p.idEstado) !== idEstado) return;
-      
       const imgNum = p.id || docId;
       const sinopsis = p.Sinopsis ? p.Sinopsis.substring(0, 180) + "..." : "";
       const youtubeLink = p.Link ? `https://www.youtube.com/v/${p.Link}` : "#";
@@ -77,12 +80,16 @@ async function getPeliculas() {
                 <img src="img/varios/btn-mas-info.jpg" width="120" height="30" alt="Ver info"/>
               </a>
             </div>
-            ${p.Link ? `
+            ${
+              p.Link
+                ? `
             <div class="boton-pelicula">
               <a href="${youtubeLink}" target="_blank">
                 <img src="img/varios/btn-trailer.jpg" width="120" height="30" alt="Ver trailer"/>
               </a>
-            </div>` : ""}
+            </div>`
+                : ""
+            }
           </div>
           <img src="img/pelicula/${imgNum}.jpg" width="160" height="226"
                onerror="this.src='img/varios/btn-mas-info.jpg'"/><br/><br/>
@@ -91,7 +98,6 @@ async function getPeliculas() {
     });
 
     contenedor.innerHTML = html;
-
   } catch (error) {
     console.error("Error al obtener películas desde Firebase:", error);
     contenedor.innerHTML = `<br/><h1>${titulo}</h1><br/><p>Error al cargar las películas.</p>`;
